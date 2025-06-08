6c9eaec1d0409f4de315d30c9e694ba707e5edd0
Simon Træls Ravn <traels@traels.it>
2025-06-08 13:22:12 +0200

    Chore. Remove all old code.

--- lib/sliders.rb
+++ lib/sliders.rb
@@ -1,12 +1,6 @@
 require "sliders/version"
 require "sliders/engine"
 require "rails/generators"
-require "rails_ext/controller_generator"
-require "rails_ext/scaffold_controller_generator"
-require "rails_ext/helper_generator"
-require "rails_ext/job_generator"
-require "rails_ext/mailer_generator"
-require "rails_ext/active_record_model_generator"
 
 module Sliders
   # Your code goes here...
